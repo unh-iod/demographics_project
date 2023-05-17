53dd7973dbbf3147354b80efd7e074026b259728
unhiod <nate.thomas@unh.edu>
2023-05-17 00:49:06 +0000

    update

--- gcs_interface.R
+++ gcs_interface.R
@@ -1,17 +1,14 @@
 cloud_prep <- function(){
   library(googleAuthR)
+  gar_auth()
   getwd()
-  if(Sys.getenv("DOMAINNAME") == "MSI"){
-    Sys.setenv("GCS_DEFAULT_BUCKET" = "demographics-384020",
-               "GCS_AUTH_FILE" = paste(str_replace_all(getwd(),"/","\\\\"),"\\demographics-384020-18095ab60914.json",sep=""))
-  }
   if(Sys.getenv("DOMAINNAME") != "MSI"){
     Sys.setenv("GCS_DEFAULT_BUCKET" = "demographics-384020",
                "GCS_AUTH_FILE" = paste(getwd(),"/","/demographics-384020-18095ab60914.json",sep=""))
   }
   library(googleCloudStorageR)
   buckets <- gcs_list_buckets("demographics-384020")
-  bucket <- buckets$name[1]
+  bucket <- buckets$name[2]
   objects <- gcs_list_objects(bucket)
   return(list(bucket = bucket,objects = objects))
 }
@@ -26,4 +23,9 @@ get_object <- function(objname,bucket){
 send_object <- function(objname,bucket,name){
   gcs_upload(objname, bucket = bucket, name=name,
              predefinedAcl='bucketLevel')
+}
+
+
+delete_object <- function(objname,bucket){
+  gcs_delete_object(objname, bucket)
 }
